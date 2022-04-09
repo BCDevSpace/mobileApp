5d841143c4f6367a9fda48dc44590db0817a025e
BCDevSpace <subodh.sasidharan.0068@gmail.com>
2022-04-09 06:59:25 +0100

    changes with ignore

---
+++ app2.js
@@ -0,0 +1,8 @@
+<html>
+  <body>
+    <h2>JS Main</h2>
+    
+    <p id="main">JS Changes</p>
+    <button type="button" onClick='document.getElementByID("main").innerHTML = "Hey welcome user">Submit</button>
+  </body>
+</html>
